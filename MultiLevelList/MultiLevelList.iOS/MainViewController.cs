8e3e4e67191df83edcf110f78d05efd21d8708ba
Sushant <apple@Apples-Mac-mini.local>
2015-11-18 19:46:52 +0530

    added ios project

---
+++ MultiLevelList/MultiLevelList.iOS/MainViewController.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+
+using Foundation;
+using UIKit;
+using MultiLevelList.Core.ViewModel;
+
+namespace MultiLevelList.iOS
+{
+	public partial class MainViewController : UIViewController
+	{
+		public MainViewController () : base ("MainViewController", null)
+		{
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			var vm = new MainViewModel ();
+			MyTableView.RegisterNibForCellReuse (UINib.FromName (MyTableViewCell.Key,
+				NSBundle.MainBundle),MyTableViewCell.Key);
+			
+			MyTableView.Source = new MyTableViewSource (vm.Items);
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+	}
+}
+
---
+++ MultiLevelList/MultiLevelList.iOS/MyTableViewCell.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+
+using Foundation;
+using UIKit;
+using System.Collections.Generic;
+using MultiLevelList.Core.Model;
+
+namespace MultiLevelList.iOS
+{
+	public partial class MyTableViewCell : UITableViewCell
+	{
+		public static readonly UINib Nib = UINib.FromName ("MyTableViewCell", NSBundle.MainBundle);
+		public static readonly NSString Key = new NSString ("MyTableViewCell");
+
+		public MyTableViewCell (IntPtr handle) : base (handle)
+		{
+		}
+
+		public TreeItem Model 
+		{
+			get;
+			set;
+		}
+
+		public override void LayoutSubviews ()
+		{
+			base.LayoutSubviews ();
+			LabelTitle.Text = Model.Title;
+		}
+	}
+}
+
